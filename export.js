ec94321566fe4170e3a39eb7b19a3b13890f56e0
Jorge Izquierdo <jorge@izqui.me>
2016-11-01 21:32:24 -0700

    Initial

---
+++ export.js
@@ -0,0 +1,11 @@
+Promise.allProperties = (object) => {
+  const keys = Object.keys(object);
+
+  return Promise.all(keys.map((k) => return object[k]))
+    .then((values) => {
+      return values.reduce((o, p, i) => {
+        o[keys[i]] = p;
+        return o;
+      }, object);
+    });
+}
