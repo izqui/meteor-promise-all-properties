24bb47bbbc7570371aa1d6d455bb32cc66989d50
Jorge Izquierdo <jorge@izqui.me>
2016-11-01 21:33:38 -0700

    Add braces

--- export.js
+++ export.js
@@ -1,7 +1,7 @@
 Promise.allProperties = (object) => {
   const keys = Object.keys(object);
 
-  return Promise.all(keys.map((k) => return object[k]))
+  return Promise.all(keys.map((k) => { return object[k] }))
     .then((values) => {
       return values.reduce((o, p, i) => {
         o[keys[i]] = p;
